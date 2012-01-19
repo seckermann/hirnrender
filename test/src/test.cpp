083aa59cd831535572b19f7b7010fdf30083e57c
Your Name <angizia89@gmail.com>
2012-01-19 15:23:37 +0100

    bug fix

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -34,7 +34,6 @@ int main(int argc, char **argv) {
   switch(argc){
     case 3:
       if (strcmp(argv[1],"-in")==0){
-        printf("hier1\n");
         image= argv[2];
         activity = NULL;
       }else{
@@ -44,12 +43,10 @@ int main(int argc, char **argv) {
       break;
     case 5:
       if (strcmp(argv[1],"-in")==0 && strcmp(argv[3],"-z")==0){
-        printf("hier2\n");
         image= argv[2];
         activity = argv[4];
       }else{
         if (strcmp(argv[1],"-z")==0 && strcmp(argv[3],"-in")==0){
-          printf("hier3\n");
           activity = argv[2];
           image= argv[4];
         }else{
@@ -65,20 +62,25 @@ int main(int argc, char **argv) {
 
 
 void renderImage(char* image, char *activity){
-  std::list<data::Image> images = data::IOFactory::load(image);
   std::list<vtkImageData*> ids;
+  std::list<vtkImageData*> ads;
+  vtkImageData* ad;
+  vtkImageData* id;
+  //Image
+  std::list<data::Image> images = data::IOFactory::load(image);
   for (std::list<data::Image>::const_iterator image = images.begin(); image != images.end(); ++image){
     ids.push_back(getImageData(*image));
   }
-  vtkImageData* id = ids.front();
+  id = ids.front();
 
+
+  //Activity
   if(activity!= NULL){
     std::list<data::Image> activities = data::IOFactory::load(activity);
-    std::list<vtkImageData*> ads;
     for (std::list<data::Image>::const_iterator activity = activities.begin(); activity != activities.end(); ++activity){
-      ids.push_back(getImageData(*activity));
+      ads.push_back(getImageData(*activity));
     }
-    vtkImageData* ad = ads.back();
+    ad = ads.front();
   }
 
 
@@ -114,28 +116,29 @@ void renderImage(char* image, char *activity){
   propertyBrain->SetIndependentComponents(true);
 
   propertyBrain->SetInterpolationTypeToLinear();
-  //propertyBrain->SetInterpolationTypeToNearest();
-  //mapper->SetInterpolationModeToCubic();
   volume->SetProperty( propertyBrain );
   volume->SetMapper(mapper);
   renderer->AddVolume(volume);
   bounds = volume->GetBounds();
-  for(int i= 0 ; i<6 ; i++ ){
-    printf("%f\n ", bounds[i]);
-  }
-  mapper->SetCroppingRegionPlanes(bounds[0],(bounds[1]-bounds[0])/2 + bounds[0], bounds[2],bounds[3],bounds[4],bounds[5]);
-  //mapper->CroppingOn();
+  double* cropping = new double[6];
+  cropping[0] = bounds[0];
+  cropping[1] = (bounds[1]-bounds[0])/2+bounds[0];
+  cropping[2] = bounds[2];
+  cropping[3] = bounds[3];
+  cropping[4] = bounds[4];
+  cropping[5] = bounds[5];
+  mapper->SetCroppingRegionPlanes(cropping[0],cropping[1], cropping[2],cropping[3],cropping[4],cropping[5]);
+  mapper->CroppingOn();
 
   //Activity
   if(activity!=NULL){
-    id = ids.back();
 
     double max = -100;
     double min = +100;
-    for (int z=0; z<id->GetDimensions()[2]; z++){
-      for (int y=0; y<id->GetDimensions()[1]; y++){
-        for (int x=0; x<id->GetDimensions()[0]; x++){
-          double temp = id->GetScalarComponentAsDouble(x,y,z,0);
+    for (int z=0; z<ad->GetDimensions()[2]; z++){
+      for (int y=0; y<ad->GetDimensions()[1]; y++){
+        for (int x=0; x<ad->GetDimensions()[0]; x++){
+          double temp = ad->GetScalarComponentAsDouble(x,y,z,0);
           if(temp > max){
             max = temp;
           }
@@ -145,20 +148,19 @@ void renderImage(char* image, char *activity){
         }
       }
     }
-    printf("%f, %f\n", min, max);
     volume = vtkVolume::New();
 
     mapper = vtkSmartVolumeMapper::New();
-    mapper->SetInput(id);
+    mapper->SetInput(ad);
     propertyBrain = vtkVolumeProperty::New();
 
     colorFun =vtkColorTransferFunction::New();
     propertyBrain->SetColor( colorFun );
-    colorFun->AddRGBPoint(	 -4,   0,   0, 0.2);
+    colorFun->AddRGBPoint(	 -4,   0,   1, 1);
     colorFun->AddRGBPoint(	 -0.2, 0,   0, 1  );
     colorFun->AddRGBPoint(	  0,   0,   0, 0  );
     colorFun->AddRGBPoint(	  0.2, 1,   0, 0  );
-    colorFun->AddRGBPoint(	  4,   0.2, 0, 0  );
+    colorFun->AddRGBPoint(	  4,   1, 1, 0  );
 
     opacityFun = vtkPiecewiseFunction::New();
     propertyBrain->SetScalarOpacity( opacityFun );
@@ -173,16 +175,14 @@ void renderImage(char* image, char *activity){
     propertyBrain->SetIndependentComponents(true);
 
     propertyBrain->SetInterpolationTypeToLinear();
-    //propertyBrain->SetInterpolationTypeToNearest();
-    //mapper->SetInterpolationModeToCubic();
     volume->SetProperty( propertyBrain );
     volume->SetMapper(mapper);
     renderer->AddVolume(volume);
 
     bounds = volume->GetBounds();
 
-    mapper->SetCroppingRegionPlanes(bounds[0],(bounds[1]-bounds[0])/2 + bounds[0], bounds[2],bounds[3],bounds[4],bounds[5]);
-   // mapper->CroppingOn();
+  mapper->SetCroppingRegionPlanes(cropping[0],cropping[1], cropping[2],cropping[3],cropping[4],cropping[5]);
+     mapper->CroppingOn();
 
 
 
@@ -231,7 +231,7 @@ vtkImageData* getImageData(data::Image image){
   const util::fvector4 spacing = image.getPropertyAs<util::fvector4>( "voxelSize" );
   id->SetSpacing(spacing[0],spacing[1],spacing[2]);
   const util::fvector4 origin = image.getPropertyAs<util::fvector4>( "indexOrigin" );
-  id->SetOrigin(origin[0]+spacing[0]/2.0,origin[1]+spacing[1]/2.0,origin[2]+spacing[2]/2.0);
+  id->SetOrigin(origin[0]-spacing[0]/2.0,origin[1]-spacing[1]/2.0,origin[2]-spacing[2]/2.0);
 
   for (int z=0; z<dims[2]; z++){
     for (int y=0; y<dims[1]; y++){
