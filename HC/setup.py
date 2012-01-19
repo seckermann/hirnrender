07f6d4031c6490ded4dfd7d48a75cd3bc20e9a4f
Hans-Christian Heinz <hheinz@rhenium.cbs.mpg.de>
2012-01-19 17:36:37 +0100

    Python Kram gemacht

--- HC/setup.py
+++ HC/setup.py
@@ -1,4 +1,6 @@
 from distutils.core import setup, Extension
 
-modul = Extension("chiffre", sources=["chiffre.cpp"])
-setup(name = "PyChiffre", version = "1.0", description = "Module for encryption techniques.", ext_modules = [modul])
+modul = Extension("isisReader", sources=["isisReader.cpp"], include_dirs=["/home/raid1/hheinz/VTKBuild/include/vtk-5.9/", "/usr/include/isis/"], define_macros = [('_ENABLE_LOG','1'),('_ENABLE_DEBUG','1')])
+setup(name = "PyIsisReader", version = "0.1", description = "Simple parser for Isis images for use with vtk", ext_modules = [modul])
+
+# , libraries = ["libisis_core.so"],  library_dirs = ["/usr/lib/"]
